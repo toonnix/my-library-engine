1bf66ff0f042201055bc39bdb3a9b43a84fd4daf
Sagsun Disman <sagsun@kitchenup.com>
2017-05-23 17:01:31 +0700

    use prod env

--- src/config.ts
+++ src/config.ts
@@ -11,11 +11,18 @@ config.database = {
     database: 'mylibrary',
     username: 'librarian',
     password: 'admin',
-    host: process.env.process.env.DATABASE_URL || 'localhost',
+    host: process.env.DATABASE_URL || 'localhost',
     port: process.env.POSTGRES_PORT || 5432,
 };
 
 if (PRODUCTION) {
-    // for example
+    config.database = {
+        "dialect": "postgres",
+        "database": "dund0u3u59u1v",
+        "username": "mhifnkruoxymho",
+        "password": "6a2fc6364e279637a362c51ea08c168de688fb3e1e60e866aa144b26be573e72",
+        "host": "ec2-54-83-26-65.compute-1.amazonaws.com",
+        "port": 5432
+    }
     config.express.ip = '0.0.0.0'
 }
